a5393977461d75dd8ba53e8bd622d0a8a2050d2d
chea-young <63410509+chea-young@users.noreply.github.com>
2021-06-16 20:50:27 +0900

    feat: add 24.py

--- 7주차/파이썬 기초 - 24..py
+++ 7주차/파이썬 기초 - 24..py
@@ -1,8 +1,10 @@
 #%% 참고 교재: 데이터 분석을 위한 판다스 입문 p.132 ~ p.136
 #%% 1. merge메서드 사용하기
 # merge메서드를 사용하여 좀 더 원하는 형태로 데이터를 연결하여 봅시다.
-
 import pandas as pd
+import sys 
+
+sys.path.append('/src/GG') # NOTE absolute path
 
 person=pd.read_csv('survey_person.csv')
 site=pd.read_csv('survey_site.csv')
@@ -10,15 +12,39 @@ survey=pd.read_csv('survey_survey.csv')
 visited=pd.read_csv('survey_visited.csv')
 
 visited_subset=visited.loc[[0,2,6],] # 특정 index만 갖는 일부 데이터 생성
+# left_on, right_on : 각 join할 key (서로 다른 key 값을 갖을 경우)
 o2o_merge=site.merge(visited_subset,left_on='name',right_on='site')
 
-# 위의 코드는 'site'의 'name'과 'visited_subset'의 'site' 이 동일한 index를 찾아서
+print(visited)
+print('---------------------------')
+print(visited_subset)
+print('============================')
+
+print(site)
+print('---------------------------')
+print(o2o_merge)
+print('============================')
+#%% 위의 코드는 'site'의 'name'과 'visited_subset'의 'site' 이 동일한 index를 찾아서
 # 'site'를 기준으로 'visited_subset'에 있는 값을 붙여넣어라는 의미
 # 몇 가지 더 해봅시다. 
-ps=person.merge(survey,left_on='ident',right_on='person')
+ps=person.merge(survey,left_on='ident',right_on='person') # left_on은 person, right_on은 survey
 vs=visited.merge(survey,left_on='ident',right_on='taken')
 
-# left_on, right_on에 전달하는 값은 여려개도 가능
+print(survey)
+print('---------------------------')
+print(person)
+print('---------------------------')
+print(visited)
+print('---------------------------')
+print(ps)
+print('============================')
+print(vs)
+print('============================')
+
+#%% left_on, right_on에 전달하는 값은 여려개도 가능
 ps_vs = ps.merge(vs,left_on=['ident','taken','quant','reading'],right_on=['person','ident','quant','reading'])
 # merge메서드의 left 데이터프레임과 right 데이터프렘임에 동일한 변수가 있다면
 # 변수명 뒤에 '_x' 와 '_y'를 붙여 구분하여 표시
+
+print(ps_vs)
+# %%
--- 7주차/파이썬 기초 - 25.py
+++ 7주차/파이썬 기초 - 25.py
@@ -3,36 +3,43 @@
 # 누락값은 값이 존재하지 않는 상태를 의미로서 NaN, NAN, nan과 같은 방법으로 표기
 # 누락값을 사용하려면 numpy라이브러리가 필요
 from numpy import NaN, NAN, nan
-print(NaN=True)
-print(NaN=False)
-print(NaN=0)
-print(NaN='')
-print(NaN=NaN)
-print(NaN=nan)
-print(NaN=NAN)
-print(nan=NAN)
 
+print(NaN==True)
+print(NaN==False)
+print(NaN==0)
+print(NaN=='')
+print(NaN==NaN)
+print(NaN==nan)
+print(NaN==NAN)
+print(nan==NAN)
+
+print("===============================")
 # 판다스에서 누락값 확인방법
 import pandas as pd
-print(pd.isnul(NaN)) # isnul(): nul이지? 응, nul이야
-print(pd.isnul(nan)) # isnul(): nul이지? 응, nul이야
-print(pd.isnul(NAN)) # isnul(): nul이지? 응, nul이야
+print(pd.isnull(NaN)) # isnul(): nul이지? 응, nul이야
+print(pd.isnull(nan)) # isnul(): nul이지? 응, nul이야
+print(pd.isnull(NAN)) # isnul(): nul이지? 응, nul이야
 # 판다스에서 누락값이 아닌 것 확인방법
-print(pd.notnul(NaN)) # notnul(): nul 아니지? 아니야, nul이야
-print(pd.notnul(42)) # notnul(): nul 아니지? 응, nul아니야
-print(pd.notnul('mising'))# notnul(): nul 아니지? 응, nul아니야
+print(pd.notnull(NaN)) # notnul(): nul 아니지? 아니야, nul이야
+print(pd.notnull(42)) # notnul(): nul 아니지? 응, nul아니야
+print(pd.notnull('mising'))# notnul(): nul 아니지? 응, nul아니야
+
 #%% 2. 누락값이 생기는 이유
+
 #%% 2-1. 누락값이 있는 데이터를 연결하는 경우
 visited=pd.read_csv('survey_visited.csv')
 survey=pd.read_csv('survey_survey.csv')
 vs=visited.merge(survey,left_on='ident',right_on='taken')
+
 #%% 2-2. 데이터 입력시 누락값을 입력한 경우
 num_legs=pd.Series({'goat': 4, 'amoeba':nan})
+
 #%% 2-3. 법위를 지정하여 데이터를 추출할 때 누락값이 생기는 경우
 gapminder=pd.read_csv('gapminder.tsv', sep='\t')
 life_exp=gapminder.groupby(['year'])['lifeExp'].mean()
 life_exp2=life_exp.loc[range(200,2010),]
 life_exp3=life_exp[life_exp.index>200]
+
 #%% 3. 누락값의 갯수 세기
 ebola=pd.read_csv('country_timeseries.csv')
 # 누락값이 아닌 개수 세기
@@ -46,6 +53,7 @@ ebola_nan=ebola.isnul()
 print(np.count_nonzero(ebola_nan))
 # 누락값 세기 3: 판다스 value_counts 메서드 사용
 ebola_Guinea_value_counts=ebola.Cases_Guinea.value_counts(dropna=False)
+
 #%% 4. 누락값 처리하기
 #%% 4-1. 누락값 변경하기
 # filna()메서드 사용해서 누락값을 0으로 변경
@@ -60,6 +68,7 @@ ebola_subset_interpolate=ebola_subset.interpolate() # backward fil
 
 #%% 4-2. 누락값 삭제하기
 ebola_dropna=ebola.dropna() # 누락값 하나라도 있는 행은 모두 삭제
+
 #%% 5. 누락값이 포함된 데이터 계산하기
 # 누락값 + 숫자 = 누락값
 ebola_subset['Cases_multiple']=ebola_subset['Cases_Guinea']+ebola_subset['Cases_Liberia']+ebola_subset['Cases_SieraLeone']
