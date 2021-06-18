8cfd955a144ebd526e265c1b365bab69a4b41a0f
chea-young <lchy0413@gmail.com>
2021-06-18 23:02:48 +0900

    fix: note 37.py

--- 10주차/37.py
+++ 10주차/37.py
@@ -5,7 +5,11 @@
 #%% 1. 에볼라 최초 발병일 계산하기
 import pandas as pd
 ebola=pd.read_csv('country_timeseries.csv')
-ebola['date_dt']=pd.to_datetime(ebola['Date'])
+print(ebola[:5])
+print('-------------1-------------')
+ebola['date_dt']=pd.to_datetime(ebola['Date']) # NOTE Date를 datetime type으로 추가
+print(ebola[:5])
+
 # 에볼라 최초발병일 구하기
 ebola_outbreak_day=ebola['date_dt'].min() # 문자열이라면 불가능
 ebola['ebola_elapsed_days']=ebola['date_dt']-ebola_outbreak_day
@@ -18,8 +22,8 @@ banks=pd.read_csv('banklist.csv', parse_dates=['Closing Date','Updated Date'])
 print(banks.info())
 
 #%% dt접근자와 quarter속성을 이용하여 은행이 파산한 분기를 계산
-banks['closing_quarter'], banks['closing_year']=(banks['Closing Date'].dt.quarter, 
-banks['Closing Date'].dt.year)
+banks['closing_quarter'], banks['closing_year']=(banks['Closing Date'].dt.quarter, banks['Closing Date'].dt.year)
+
 # groupby메서드를 사용하여 연도별로 파산한 은행의 개수를 계산
 closing_year=banks.groupby(['closing_year']).size()
 closing_year_q=banks.groupby(['closing_year','closing_quarter']).size()
@@ -36,7 +40,9 @@ plt.show()
 import pandas as pd
 tesla=pd.read_csv('tesla_stock_quandl.csv',parse_dates=[0])
 # 2010년 6월 데이터만 추출해봅시다.
+print('-------------1-------------')
 print(tesla.loc[(tesla.Date.dt.year==2010) & (tesla.Date.dt.month==6)])
+
 # datetime오브젝트를 데이터프레임의 index로 설정하면 원하는 시간의 데이터를 편리하게 추출
 # 2010년 6월 데이터만 추출해봅시다. print(tesla.loc[(tesla.Date.dt.year==2010) & (tesla.Date.dt.month==6)])
 # datetime오브젝트를 데이터프레임의 index로 설정하면 원하는 시간의 데이터를 편리하게 추출
@@ -44,13 +50,28 @@ tesla.index=tesla['Date']
 tesla_2015=tesla.loc['2015']
 tesla_201006=tesla.loc['2010-06']
 tesla_20151231=tesla.loc['2015-12-31']
+print('-------------2-------------')
+print(tesla[:10])
+print('-------------3-------------')
+print(tesla_2015)
+print('-------------4-------------')
+print(tesla_201006)
 
 #%% 4. 시간 범위와 인덱스
 import pandas as pd
 ebola=pd.read_csv('country_timeseries.csv',parse_dates=[0])
 # date_range메서드를 사용하여 2014년 12월 31일부터 2015년 1월 5이 사이의 시간 인덱스 생성
 head_range=pd.date_range(start='2014-12-31', end='2015-01-05')
+
 # 시간 범위의 주기 설정할 수 있는 freq속성값은 p.263 참고
 ebola_5=ebola.head()
 ebola_5.index=ebola_5['Date']
 ebola_5=ebola_5.reindex(head_range)
+
+print('-------------1-------------')
+print(ebola_5)
+print('-------------2-------------')
+print(ebola_5)
+print('-------------3-------------')
+print(ebola_5)
+# %%
